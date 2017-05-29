53f19cc12d045f22fbbf573cd34f5db70c93f362
Horvath Gergo <gege32@gmail.com>
2017-05-29 17:28:16 +0200

    first commit

---
+++ src/main/java/hu/horvathg/hobbycsere/HobbycsereApplication.java
@@ -0,0 +1,12 @@
+package hu.horvathg.hobbycsere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbycsereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbycsereApplication.class, args);
+	}
+}

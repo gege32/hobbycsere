7f87d188f8f98110710e821d16c335bc4168106c
Horvath Gergo <gehorvath@gehorvath.horvathgergo.net>
2017-06-04 23:31:58 +0200

    aergsetbsxdrtfgbdxrtgb

--- src/main/java/hu/horvathg/hobbycsere/model/messages/Message.java
+++ src/main/java/hu/horvathg/hobbycsere/model/messages/Message.java
@@ -1,5 +1,15 @@
 package hu.horvathg.hobbycsere.model.messages;
 
+import hu.horvathg.hobbycsere.model.user.User;
+
 public class Message {
+	
+	String message;
+	
+	long timestamp;
+	
+	User from;
+	
+	User to;
 
 }
